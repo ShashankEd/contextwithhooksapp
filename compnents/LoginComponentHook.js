531c31bb3db6f7ef67335a5e7d8b4cfd113ea78d
Shashank Gupta <shashankgupta@TSM-SHASHANK.local>
2021-05-23 14:52:24 +0530

    useContext, and setContext implementation

---
+++ compnents/LoginComponentHook.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect,useContext } from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
+import {newContext} from '../App'
+function LoginComponentHook() {
+    const [contextstore, setContextStore] = useState(useContext(newContext))
+    const[name,setName] = useState();
+    const[status,setStatus] = useState()
+    useEffect(() => {
+        console.log("context store ", contextstore);
+        if(contextstore) {
+            setStatus(contextstore['status']);
+            setName(contextstore['name']);
+        }
+    })
+    return (
+        <View>
+            <Text style={{ fontSize:30 }}> Reading from context</Text>
+            <Text style={{ fontSize: 15 }}>Name:  {name +" Status: " + status}</Text>
+        </View>
+    )
+}
+
+export default LoginComponentHook;
